40aadae07f240e1262acdb4c98cf9d56f01a78cf
soniyaasekar98 <34702909+soniyaasekar98@users.noreply.github.com>
2018-06-19 21:51:42 +0530

    Update and rename ss.java to dd.java

---
+++ dd.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class dd
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int s1=nextInt();
+for(int i=0;i<s1;i++)
+{
+System.out.println("hello");
+}
+}
+}
