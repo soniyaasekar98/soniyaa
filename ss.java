d3dcc980368944ddb6ad5fa65330f684cfca0e89
soniyaasekar98 <34702909+soniyaasekar98@users.noreply.github.com>
2018-06-19 21:50:54 +0530

    Create ss.java

---
+++ ss.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class ss
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=nextInt();
+for(int i=0;i<n;i++)
+{
+System.out.println("hello");
+}
+}
+}
