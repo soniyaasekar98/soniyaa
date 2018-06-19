734f2100f3aab0f48d771ae088c8005c4a7e03e5
soniyaasekar98 <34702909+soniyaasekar98@users.noreply.github.com>
2018-06-19 22:01:18 +0530

    Create oo.java

---
+++ oo.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class oo
+{
+    public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int k=sc.nextInt();
+       for(int x=1;x<=k;x++)
+       {
+       System.out.println("hello");
+       }
+    }
+}
