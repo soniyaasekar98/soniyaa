a9a15030751c8c0f710e85d83f33307981c2846b
soniyaasekar98 <34702909+soniyaasekar98@users.noreply.github.com>
2018-06-20 21:23:31 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class sum
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int sum=0;
+for(int i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.print(sum);
+}
+}
