722d0607b3a2269df57fe3f1c360712ba9dac68f
soniyaasekar98 <34702909+soniyaasekar98@users.noreply.github.com>
2018-07-07 10:15:32 +0530

    Create five.java

---
+++ five.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class five
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int b;
+for(i=1;i<=5;i++)
+{
+b=i*n;
+System.out.print(b+" ");
+}
+}
+}
